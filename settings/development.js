54a3bd4eb6ecc4305e5a27c805514a2149236b89
Lauren Langford <60724991+laurendoesntlikeusernames@users.noreply.github.com>
2022-06-16 19:46:38 +0800

    not for git

--- settings/development.js
+++
@@ -1,7 +0,0 @@
-const settings = {
-    googleMaps: {
-      apiKey: "AIzaSyD5mR9ro2VfA-NV8thaKBUGfyl0Xx-XY1w",
-    }
-};
-
-export default settings;
--- settings/index.js
+++
@@ -1,9 +0,0 @@
-import development from "./development";
-import production from "./production";
-
-const settings = {
-  development,
-  production,
-}[process.env.NODE_ENV];
-
-export default settings;
